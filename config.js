283a57eb27394d111bf8cd50d202e51a31080709
Daniel Hammerin <dh222gh@student.lnu.se>
2021-02-06 12:03:18 +0100

    Added some more item URLs to the scraper config list.

--- config.js
+++ config.js
@@ -11,7 +11,20 @@ PRICE_CONSTANTS = {
 URLS = [
     'http://services.runescape.com/m=itemdb_oldschool/Old+school+bond/viewitem?obj=13190',
     'http://services.runescape.com/m=itemdb_oldschool/Kodai+wand/viewitem?obj=21006',
-    'https://secure.runescape.com/m=itemdb_oldschool/Ghrazi+rapier/viewitem?obj=22324'
+    'https://secure.runescape.com/m=itemdb_oldschool/Ghrazi+rapier/viewitem?obj=22324',
+    'https://secure.runescape.com/m=itemdb_oldschool/Twisted+bow/viewitem?obj=20997',
+    'https://secure.runescape.com/m=itemdb_oldschool/Scythe+of+vitur+%28uncharged%29/viewitem?obj=22486',
+    'https://secure.runescape.com/m=itemdb_oldschool/Ancestral+hat/viewitem?obj=21018',
+    'https://secure.runescape.com/m=itemdb_oldschool/Ancestral+robe+top/viewitem?obj=21021',
+    'https://secure.runescape.com/m=itemdb_oldschool/Ancestral+robe+bottom/viewitem?obj=21024',
+    'https://secure.runescape.com/m=itemdb_oldschool/Armadyl+helmet/viewitem?obj=11826',
+    'https://secure.runescape.com/m=itemdb_oldschool/Armadyl+chestplate/viewitem?obj=11828',
+    'https://secure.runescape.com/m=itemdb_oldschool/Armadyl+chainskirt/viewitem?obj=11830',
+    'https://secure.runescape.com/m=itemdb_oldschool/Armadyl+crossbow/viewitem?obj=11785',
+    'https://secure.runescape.com/m=itemdb_oldschool/Bandos+chestplate/viewitem?obj=11832',
+    'https://secure.runescape.com/m=itemdb_oldschool/Bandos+tassets/viewitem?obj=11834',
+    'https://secure.runescape.com/m=itemdb_oldschool/Saradomin+godsword/viewitem?obj=11806',
+    'https://secure.runescape.com/m=itemdb_oldschool/Pegasian+boots/viewitem?obj=13237',
 ]
 
 module.exports = {
